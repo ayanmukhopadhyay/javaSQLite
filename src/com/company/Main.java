df761a5c7bd4a8fd3d68a2cef423835aed435b3f
ayanmukhopadhyay <ayan.mukhopadhyay@vanderbilt.edu>
2016-07-28 15:27:19 -0500

    Initial Commit with Creation of DB and Table

---
+++ src/com/company/Main.java
@@ -0,0 +1,53 @@
+package com.company;
+
+import java.sql.*;
+
+
+public class Main {
+
+    public static void createDB()
+    {
+        Connection c = null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:test.db");
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+        System.out.println("Opened database successfully");
+    }
+
+    public static void createTable()
+    {
+        Connection c = null;
+        Statement stmt = null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:test.db");
+            System.out.println("Opened database successfully");
+
+            stmt = c.createStatement();
+            String sql = "CREATE TABLE AVLData " +
+                    "(Datetime TEXT NOT NULL, " +
+                    " CarID TEXT NOT NULL, " +
+                    " Lat REAL, " +
+                    " Long REAL)";
+            stmt.executeUpdate(sql);
+            stmt.close();
+            c.close();
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+        System.out.println("Table created successfully");
+    }
+
+
+
+    public static void main(String[] args) {
+	// write your code here
+        createDB();
+        createTable();
+    }
+}
